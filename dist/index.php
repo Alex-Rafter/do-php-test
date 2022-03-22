d6e7ef0a72d3491e87a9c1725fe0ed34ff68e4c0
Alex-Rafter <you@example.com>
2022-03-22 15:37:45 +0000

    test get req

--- dist/index.php
+++ dist/index.php
@@ -1,4 +1,6 @@
 <?php
-$envTest = "123456789";
-echo "$envTest";
+$fetch = file_get_contents("https://api.sheety.co/7016cabf6b37601c93f0bcbd5ec85980/testForWeb/formResponses1");
+// $envTest = "123456789";
+// echo "$envTest";
+echo "$fetch";
 ?>
