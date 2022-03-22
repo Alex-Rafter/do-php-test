00c73d3e8b212c74ca12960ac7105c64418927a3
Alex Rafter <alexrafteremail@gmail.com>
2022-03-22 20:28:40 +0000

    addedd header

--- dist/index.php
+++ dist/index.php
@@ -1,4 +1,5 @@
 <?php
+header("Access-Control-Allow-Origin: *");
 $fetch = file_get_contents("https://api.sheety.co/7016cabf6b37601c93f0bcbd5ec85980/testForWeb/formResponses1");
 // $envTest = "123456789";
 // echo "$envTest";
